d7a4d91799371fe6ca20328f1f62726e04930e34
PranavShewale44 <pranavprashantshewale@gmail.com>
2025-03-07 23:09:58 +0530

    minor changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for # type: ignore
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
